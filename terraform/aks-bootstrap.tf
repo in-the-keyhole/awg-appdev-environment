6ae98f17a5504696956154c33947fb5c27d31a9b
Jerome Haltom <jhaltom@alethic.solutions>
2025-04-20 19:28:46 -0500

    Add privateSubnet to env config. Add AKS monitoring.

--- terraform/aks-bootstrap.tf
+++ terraform/aks-bootstrap.tf
@@ -13,7 +13,7 @@ locals {
     release_name = var.release_name
     default_tags = var.default_tags
     platform_registry = data.azurerm_container_registry.platform
-    awg_appdev_version = "0.0.233"
+    awg_appdev_version = "0.0.269"
     azure_subscription_id = data.azurerm_client_config.current.subscription_id,
     crossplane_azure_identity = azurerm_user_assigned_identity.crossplane
     crossplane_azure_provider_version = "v1.11.3"
